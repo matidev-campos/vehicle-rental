0962c564b4de4648d4360cc240f641596305a4ef
matidev-campos <matidev.campos@gmail.com>
2026-01-02 12:28:16 -0300

    bootstrap spring boot project with java 21

---
+++ src/test/java/com/matidev/vehiclerental/VehicleRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matidev.vehiclerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
